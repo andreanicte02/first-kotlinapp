ff8e562415ae17065339bf099c127b4907a04071
andreanicte02 <andreanicte02@gmail.com>
2021-01-06 13:40:02 -0600

    -notas

--- app/src/main/java/com/example/firstappkotlin/text.kt
+++ app/src/main/java/com/example/firstappkotlin/text.kt
@@ -1,3 +1,31 @@
 package com.example.firstappkotlin
 
+
 fun add(x: Int, y:Int): Int = x + y
+
+abstract class Person (name: String, val age: Int){
+
+    var name = name
+    get() = "name: $field"
+
+    set(value){
+        field = value
+    }
+
+}
+
+
+
+class Developer: Person{
+
+    constructor(name: String):super(name,29)
+
+    constructor(age: Int ):super("Fanco", age)
+}
+
+fun test(){
+
+    val d = Developer("Pancho")
+
+    val name = d.name //name pancho
+}
